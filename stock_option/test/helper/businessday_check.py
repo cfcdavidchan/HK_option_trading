fc10f7d119243652558a937a7971bb72fe8796df
cfcdavidchan <cfcdavidchan@gmail.com>
2018-04-18 16:09:31 +0800

    download_zipfile.ipynb finished

--- stock_option/test/helper/businessday_check.py
+++ stock_option/test/helper/businessday_check.py
@@ -19,24 +19,25 @@ def date_to_string(date):
     return datetime_formate.strftime("%y%m%d")
 
 # Creating a list of business day.
-    # First input is the target preiod (days)
-    # Second input is the start_day of the preiod, presetted as today
-        # Note that the start_day means the last day of the whole target preiod because the function is counting backward
+    # First input is the target period (days)
+    # Second input is the start_day of the period, presetted as today
+        # Note that the start_day means the last day of the whole target period because the function is counting backward
     # Third input is the date formate inside the list, it can be in Quantlib date formate ('ql') or string ('str') formate.
-def business_day_preiod_list(period, start_day = ql.Date_todaysDate(), formate = 'ql'):
-    preiod_list = list()
+    
+def business_day_period_list(period, start_day = ql.Date_todaysDate(), formate = 'ql'):
+    period_list = list()
     
     if formate == 'str':
-        preiod_list.append(date_to_string(start_day))
+        period_list.append(date_to_string(start_day))
     else:
-        preiod_list.append(start_day)
+        period_list.append(start_day)
         
-    for i in range(1, preiod +1):
+    for i in range(1, period):
         min_period = ql.Period(-i, ql.Days)
         date = calendar.advance(start_day, min_period)
         
         if formate == 'str':
             date = date_to_string(date)
-        preiod_list.append(date)
+        period_list.append(date)
     
-    return preiod_list
+    return period_list
